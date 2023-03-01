928e054bdeaee42020e61a132b849f83ec96f798
Annie  Indreiten <devannie777@gmail.com>
2023-03-01 04:59:34 +0000

    CSS updates and favicon

--- web/js/script.js
+++ web/js/script.js
@@ -1,22 +1,4 @@
-/*NAVIGATION FOR TABLET AND MOBILE
-
-const closeBtn = document.querySelector(".close-nav");
-const openBtn = document.querySelector(".open-nav");
-const navMenu = document.querySelector(".menu-nav-mobile");
-
-//Attach eventlisteners and toggle navigation-open class
-openBtn.addEventListener("click", () => {
-  navMenu.classList.add("navigation-open");
-});
-
-closeBtn.addEventListener("click", () => {
-  navMenu.classList.remove("navigation-open");
-});
-
-
-/*
-
-/* Description: Custom JS file *
+/*NAVIGATION FOR MOBILE
 
 /* Navigation */
 const toggle = document.querySelector(".hamburger");
